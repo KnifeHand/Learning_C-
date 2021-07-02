e5ca8e69d7937bfe2d4cff829adecda6709cd6d4
Matt Hurt <32580151+KnifeHand@users.noreply.github.com>
2021-07-02 13:31:55 -0600

    Add files via upload

---
+++ SchoolApp/SchoolLibrary/School.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    class School
+    {
+        string SchoolName;
+        string SchoolAddress;
+        string SchoolCity;
+        string SchoolState;
+        string SchoolZip;
+        string PhoneNumber;
+
+        //How to initialize classes with the same name.
+        //Both classes can be used withou having any collisions.
+        //Elementary.Volume volume;
+        //HighSchool.Volume volume2;
+    }
+}
