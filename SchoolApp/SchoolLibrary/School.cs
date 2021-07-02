74d1496dbbd9da534cb77c8153508410cdb1972d
Matt Hurt <mhurt@msudenver.edu>
2021-07-02 15:08:02 -0600

    Implement auto properties

--- SchoolApp/SchoolLibrary/School.cs
+++ SchoolApp/SchoolLibrary/School.cs
@@ -6,12 +6,12 @@ namespace SchoolLibrary
 {
     class School
     {
-        string SchoolName;
-        string SchoolAddress;
-        string SchoolCity;
-        string SchoolState;
-        string SchoolZip;
-        string PhoneNumber;
+        string Name { get; set; } // Auto property uses encapsulation
+        string Address { get; set; }
+        string City { get; set; }
+        string State { get; set; }
+        string Zip { get; set; }
+        string PhoneNumber { get; set; }
 
         //How to initialize classes with the same name.
         //Both classes can be used withou having any collisions.
